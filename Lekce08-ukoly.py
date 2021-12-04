58c6beb9ba1720c7a2b42c1a120125c9a03933df
Tereza Bohatá <tb.bohata@gmail.com>
2021-12-04 22:09:51 +0100

    Lekce 08 - úkoly

--- Lekce08-ukoly.py
+++ Lekce08-ukoly.py
@@ -18,10 +18,34 @@ res = mod.fit()
 
 predictions = res.predict(start=data_csco.shape[0], end=data_csco.shape[0] + 5)
 data_forecast = pd.DataFrame(predictions, columns=["Close_prediction"])
-data_with_prediction = pd.concat([data_cisco, data_forecast])
+data_with_prediction = pd.concat([data_csco, data_forecast])
 data_with_prediction[["Close", "Close_prediction"]].tail(50).plot()
 
 print(data_with_prediction["Close"])
 plt.show()
 
-### Otázky na Python
+
+
+## Otázky na Python
+import requests
+import pandas as pd
+import matplotlib.pyplot as plt
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.holtwinters import ExponentialSmoothing
+
+r = requests.get("https://raw.githubusercontent.com/pesikj/progr2-python/master/data/MLTollsStackOverflow.csv")
+open("MLTollsStackOverflow.csv", "wb").write(r.content)
+
+data = pd.read_csv("MLTollsStackOverflow.csv")
+
+decompose = seasonal_decompose(data["python"], model='multiplicative', period=12)
+decompose.plot()
+plt.show()
+
+mod = ExponentialSmoothing(data["python"], seasonal_periods=12, trend="multiplicative", seasonal="multiplicative", initialization_method="estimated")
+res = mod.fit()
+data["HM"] = res.fittedvalues
+data_forecast = pd.DataFrame(res.forecast(12), columns=["Forecast"])
+data_with_forecast = pd.concat([data, data_forecast])
+data_with_forecast[["HM", "python", "Forecast"]].plot()
+plt.show()
